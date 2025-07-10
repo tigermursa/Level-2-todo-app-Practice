3e29a644b4d3c419ae6066bb9c6dc31e8a62efd1
Mursalin <mursalin2311@gmail.com>
2025-07-11 00:39:31 +0600

    shascn install done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-const App = () => {
-  return <div className="text-4xl text-red-600">Hello Bro</div>;
-};
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
+}
 
 export default App;
