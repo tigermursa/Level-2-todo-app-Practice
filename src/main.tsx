c620cdad10db70bbaa23bc4067283c4b569532dc
Mursalin <mursalin2311@gmail.com>
2025-07-12 01:55:12 +0600

    redux setup done

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import "./index.css";
 
 import { RouterProvider } from "react-router";
 import router from "./routes/index.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
   </StrictMode>
 );
